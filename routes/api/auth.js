b418601a31e5adcafb3e8f748d576db27a286684
Philip Rodgers <philip_rodgers@hotmail.com>
2021-03-04 12:16:27 -0600

    Added no-API, user app link, Whitelist endpoints, etc

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,57 +1,74 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var uniqueValidator = require('mongoose-unique-validator');
-var passportLocalMongoose = require('passport-local-mongoose'); 
+//Register a new user
+var express = require('express');
+var router = express.Router();
+var Users = require('../../models/users');
+var passport = require('passport');
 
-//Create a schema
-var Users = new Schema({
-  email: {
-    type: String,
-    required: [true, 'Please enter an email'],
-    unique: [true, 'Email must be unique']
-  },
-  username: {
-    type: String,
-    required: [true, 'Please enter a username'],
-    unique: [true, 'Usernames must be unique']
-  },
-  first_name: String,
-  last_name: String,
-  admin: {
-    type: Boolean,
-    default: false
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  },
-  modified: {
-    type: Date,
-    default: Date.now
-  },
-  hash: {
-    type: String,
-    required: [
-      true,   
-      'There was a problem creating your password'
-    ]
-  },
-  salt: {
-    type: String,
-    required: [
-      true, 
-      'There was a problem creating your password'
-    ]
-  },
+router.post('/register', function(req,res,next){
+  var data = req.body;
+
+  Users.register(new Users({
+    username: data.username,
+    email: data.email,
+    first_name: data.first_name,
+    last_name: data.last_name
+  }), 
+  data.password, 
+  function(err, user){
+
+    if(err){
+
+      return res.json({
+        success: false, 
+        user: req.body, 
+        errors: err
+      });
+      
+    }
+
+    return res.json({
+      success: true,
+      user: user
+    });
+
+  });
 
 });
-Users.pre('save', function(next){
-    this.modified = new Date().toISOString();
-    next();
-  });
-  
-//Add unique validation properties to the model
-Users.plugin(uniqueValidator);
-Users.plugin(passportLocalMongoose);
 
-module.exports  = mongoose.model('Users', Users);
+router.post('/login', function(req, res, next) {
+    //
+    passport.authenticate('local', function(err, user, info) {
+  
+      if (err) { 
+        return res.json({success:false, error: err});
+      }
+  
+      if (!user) {
+        return res.json({success:false, error: info.message });
+      }
+  
+      req.logIn(user, function(err) {
+  
+        if (err) { 
+          return res.json({success:false, error: err });
+        }
+  
+        //we will use a console.log() to test the session data
+        console.log(req.session);
+  
+        return res.json({success:true, user: user });
+  
+      });
+    })(req, res, next);
+  });
+
+  router.delete('/logout', function(req, res){
+    req.logout();
+    if(!req.session.passport.user){
+      return res.json({success: 'true'});
+    }else{
+      return res.json({success: 'false'});
+    }
+  });
+    
+module.exports = router;
